fb73dec5c8f3cc368c59f08a8cfb99d2366ee085
DominikCaban <caban.dominik@gmail.com>
2022-12-05 23:33:38 +0100

    upload all files

---
+++ labs/DE2_ProjectTimer/Program/src/main.c
@@ -0,0 +1,493 @@
+/***********************************************************************
+ *
+ * LCD Timer controlled by 2Axis Joystick and rotary encoder
+ * Digital Electronics 2 Team Project 1
+ * 5.12.2022
+ *
+ *
+ * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
+ *
+ *
+ * Based on code from Digital Electronics 2 Course
+ * Copyright (c) 2018 Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ *
+ **********************************************************************/
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>        // AVR device-specific IO definitions
+#include <avr/interrupt.h> // Interrupts standard C library for AVR-GCC
+#include <gpio.h>          // GPIO library for AVR-GCC
+#include "timer.h"         // Timer library for AVR-GCC
+#include <stdlib.h>        // C library. Needed for number conversions
+#include <uart.h>
+#include <lcd.h>
+
+// rotary encoder
+#define ROT_SW 2
+#define ROT_CLK 2
+#define ROT_DT 3
+
+// joystick switch pin
+#define JOY_SW 2
+
+// Global Variables
+uint8_t hours = 0, seconds = 0, minutes = 1, tenths = 0;                 // timer
+uint8_t set_hours = 0, set_seconds = 0, set_minutes = 1, set_tenths = 0; // timer
+uint8_t realx = 0, realy = 0;                                            // real x,y cursor position on lcd
+uint8_t rot_sw_state = 1, joy_sw_state = 1;                              // previos state to prevent multiple actions on single click
+#define STARTED 1
+#define STOPPED 0
+uint8_t state = STOPPED;
+
+// Initialization of LCD display
+void init_lcd()
+{
+    // Initialize display
+    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+
+    // Put string(s) on LCD screen
+    lcd_gotoxy(3, 0);
+    lcd_puts("00:00:00:0");
+    lcd_gotoxy(5, 1);
+    lcd_puts("STOPPED");
+
+    // Timer 2 is for LCD
+    TIM2_overflow_16ms();
+}
+
+// Initialization of Joystick functions (ADC)
+void init_joystick()
+{
+    // Timer1 is for starting ADC conversion
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
+
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    ADMUX |= (1 << REFS0);  // 1
+    ADMUX &= ~(1 << REFS1); // 0
+    // Select input channel ADC0 (voltage divider pin)
+    ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0)); // 0
+    // Enable ADC module
+    ADCSRA |= (1 << ADEN);
+    // Enable conversion complete interrupt
+    ADCSRA |= (1 << ADIE);
+    // Set clock prescaler to 128
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+    // set up SW button
+    DDRC &= ~(1 << JOY_SW);
+    PORTC |= (1 << JOY_SW);
+}
+
+// Initialization of Rotary encoder
+void init_encoder()
+{
+    // Initialize Rotary Encoder:
+    DDRD &= ~(1 << ROT_SW);
+    PORTD |= (1 << ROT_SW);
+
+    DDRD &= ~(1 << ROT_CLK);
+    PORTD |= (1 << ROT_CLK);
+
+    DDRD &= ~(1 << ROT_DT);
+    PORTD |= (1 << ROT_DT);
+
+    // external interrupts
+    EIMSK = 1 << INT0;
+    EICRA = 1 << ISC00;
+    EICRA &= ~(1 << ISC01);
+}
+
+void cursor_off()
+{
+    lcd_command(12);
+}
+
+void cursor_on()
+{
+    lcd_command(15);
+    lcd_gotoxy(realx, 0);
+}
+
+// refresh lcd with new values
+void refresh_lcdtime()
+{
+    char string[2];
+    cursor_off();
+    {
+        lcd_gotoxy(12, 0);
+
+        itoa(tenths, string, 10); // Convert decimal value to string
+        lcd_puts(string);
+    }
+
+    {
+        lcd_gotoxy(9, 0);
+        if (seconds < 10)
+        {
+            lcd_puts("0");
+        }
+        itoa(seconds, string, 10); // Convert decimal value to string
+        lcd_puts(string);
+    }
+    {
+        lcd_gotoxy(6, 0);
+        if (minutes < 10)
+        {
+            lcd_puts("0");
+        }
+        itoa(minutes, string, 10); // Convert decimal value to string
+        lcd_puts(string);
+    }
+
+    lcd_gotoxy(3, 0);
+    if (hours < 10)
+    {
+        lcd_puts("0");
+    }
+    itoa(hours, string, 10); // Convert decimal value to string
+    lcd_puts(string);
+    cursor_on();
+}
+
+void timer_runout(int i)
+{
+    if (i == 1)
+    {
+        PORTB |= (1 << PORTB5);
+    }
+    else if (i == 0)
+    {
+        PORTB &= ~(1 << PORTB5);
+    }
+    // if timer ran out - light up LED 
+}
+
+void start_tim()
+{
+    TIM2_overflow_interrupt_enable();
+    TIM1_overflow_interrupt_disable();
+    // disable rotary encoder interrupts
+    EIMSK &= (~1 << INT0);
+}
+
+// Start Clock Timer on LCD
+void start_timer()
+{
+    start_tim();
+    // save set values to reset back to
+    timer_runout(0);
+    set_hours = hours;
+    set_minutes = minutes;
+    set_seconds = seconds;
+    set_tenths = tenths;
+    lcd_gotoxy(5, 1);
+    lcd_puts("STARTED");
+    cursor_off();
+}
+
+void stop_tim()
+{
+    TIM2_overflow_interrupt_disable();
+    TIM1_overflow_interrupt_enable();
+    // enable rotary encoder interrupts
+    EIMSK = 1 << INT0;
+}
+
+// Stop Clock Timer on LCD
+void stop_timer()
+{
+    stop_tim();
+    lcd_gotoxy(5, 1);
+    lcd_puts("STOPPED");
+    cursor_on();
+}
+
+void reset_timer()
+{
+    hours = set_hours;
+    minutes = set_minutes;
+    seconds = set_seconds;
+    tenths = set_tenths;
+    lcd_gotoxy(5, 1);
+    lcd_puts("START  ");
+    refresh_lcdtime();
+}
+/*ADC  Channels
+    Analog0 -> VRx
+    Analog1 -> VRy
+*/
+int main(void)
+{
+    // Initialize USART to asynchronous, 8N1, 9600
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
+    uart_puts("Start of project...\n");
+    DDRB |= (1 << DDB5);
+    // Call all initialization functions
+    init_lcd();
+    init_joystick();
+    init_encoder();
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+ISR(INT0_vect)
+{
+    uint8_t clk, dt;
+    clk = (PIND & (1 << ROT_CLK)) >> ROT_CLK;
+    dt = (PIND & (1 << ROT_DT)) >> ROT_DT;
+    // SIMULIDE
+    // In reality check if clk>dt or clk<dt
+    if (clk != dt)
+    {
+        uart_puts("LEFT");
+        switch (realx)
+        {
+        case 4:
+            if (hours > 0)
+                hours--;
+            break;
+
+        case 7:
+            if (minutes > 0)
+                minutes--;
+            break;
+
+        case 10:
+            if (seconds > 0)
+                seconds--;
+            break;
+
+        case 12:
+            if (tenths > 0)
+                tenths--;
+            break;
+        }
+    }
+    else if (clk == dt)
+    {
+        uart_puts("RIGHT");
+        switch (realx)
+        {
+        case 4:
+            if (hours < 99)
+                hours++;
+            break;
+
+        case 7:
+            if (minutes < 59)
+                minutes++;
+            break;
+
+        case 10:
+            if (seconds < 59)
+                seconds++;
+            break;
+
+        case 12:
+            if (tenths < 9)
+                tenths++;
+            break;
+        }
+    }
+    refresh_lcdtime();
+}
+
+// Interrupt Vector for Timer1 overflow
+// Used for starting ADC conversion of joystick position
+ISR(TIMER1_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+    no_of_overflows++;
+
+    uint8_t joy_sw = (PINC & (1 << JOY_SW)) >> JOY_SW;
+    uint8_t rot_sw = (PINB & (1 << ROT_SW)) >> ROT_SW;
+    // Start timer
+    if (joy_sw == 0)
+    {
+        // do it only once per click
+        if (joy_sw_state == 1)
+        {
+            // otherwise start the clock
+            uart_puts("START  ");
+            start_timer();
+        }
+    }
+    joy_sw_state = joy_sw;
+
+    // Reset timer back to set time
+    if (rot_sw == 0)
+    {
+        // do it only once per click
+        if (rot_sw_state == 1)
+        {
+            // otherwise start the clock
+            uart_puts("RESET");
+        }
+    }
+    rot_sw_state = rot_sw;
+
+    if (no_of_overflows >= 3)
+    {
+        no_of_overflows = 0;
+        ADCSRA |= (1 << ADSC);
+    }
+}
+
+// Interrupt Vector for Timer2 overflow
+// Used for LCD Timer
+ISR(TIMER2_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+
+    uint8_t joy_sw = (PINC & (1 << JOY_SW)) >> JOY_SW;
+    if (joy_sw == 0)
+    {
+        // do it only once per click
+        if (joy_sw_state == 1)
+        {
+            // otherwise start the clock
+            uart_puts("STOP");
+            stop_timer();
+        }
+    }
+    joy_sw_state = joy_sw;
+
+
+    no_of_overflows++;
+    if (no_of_overflows >= 6)
+    {
+        // Do this every 6 x 16 ms = 100 ms
+        // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+        no_of_overflows = 0;
+        tenths--;
+        if (tenths > 250)
+        {
+            tenths = 9;
+            seconds--;
+            if (seconds > 250)
+            {
+                seconds = 59;
+                minutes--;
+                if (minutes > 250)
+                {
+                    minutes = 59;
+                    hours--;
+                    if (hours > 250)
+                    {
+                        // timer done
+                        tenths = 0;  // Tenths of a second
+                        seconds = 0; // Seconds of a second
+                        minutes = 0; // Minutes of a second
+                        hours = 0;
+                        stop_tim();
+                        reset_timer();
+                        timer_runout(1);
+                    }
+                }
+            }
+        }
+    }
+    refresh_lcdtime();
+}
+
+void cursor_change(int8_t x_direction, int8_t y_direction)
+{
+    char string[10];
+    static uint8_t vcursor_x = 0;
+    // static uint8_t vcursor_y=0; not needed for 2 lines, useful for multiline display
+    if (x_direction == 1 && vcursor_x < 3)
+    {
+        vcursor_x++;
+    }
+    if (x_direction == -1 && vcursor_x > 0)
+    {
+        vcursor_x--;
+    }
+    if (y_direction == 1)
+    {
+        realy = 1;
+    }
+    else if (y_direction == -1)
+    {
+        realy = 0;
+    }
+    uint8_t positions[] = {4, 7, 10, 12};
+    realx = positions[vcursor_x];
+
+    uart_puts("realx ");
+    itoa(realx, string, 10);
+    uart_puts(string);
+    uart_puts("\n ");
+    lcd_gotoxy(realx, 0);
+}
+/**********************************************************************
+ * Function: ADC complete interrupt
+ * Purpose:  Display converted value on LCD screen.
+ **********************************************************************/
+ISR(ADC_vect)
+{
+    uint16_t value;
+    static uint8_t breakpoint = 0; // 0 neni breakpoint, 1 je breakpoint
+
+    // Read converted value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
+
+    value = ADC;
+    if ((ADMUX & 7) == 0)
+    {
+        // X AXIS ADC
+        if (value > 1000)
+        {
+            if (breakpoint == 0)
+                cursor_change(1, 0);
+            breakpoint = 1;
+        }
+        else if (value < 30)
+        {
+            if (breakpoint == 0)
+                cursor_change(-1, 0);
+            breakpoint = 1;
+        }
+        else
+        {
+            // reset the breakpoint
+            breakpoint = 0;
+        }
+        // start reading y joystick position
+        // Select input channel ADC1 (Y joystick)
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1));
+        ADMUX |= (1 << MUX0);
+        // start conversion
+        ADCSRA |= (1 << ADSC);
+    }
+    else if ((ADMUX & 7) == 1)
+    {
+        // Y AXIS ADC
+        // if y value is being read
+        if (value > 1010)
+        {
+            cursor_change(0, 1);
+        }
+        else if (value < 5)
+        {
+            cursor_change(0, -1);
+        }
+        // select channel back to x input (channel 0)
+        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+    }
+}
---
+++ labs/DE2_ProjectTimer/README.md
@@ -0,0 +1,162 @@
+
+# Časovač s LCD rozhraním <br> postavený na kontroléry ATMEGA328p 
+
+Použitie analógového joysticku (2 kanály ADC, 1 tlačidlo), otočného snímača a modulu LCD Digilent PmodCLP.
+
+### Členovia týmu
+
+**Dominik Caban** <br>
+- užívateľské rozrhanie (display, joistick), návrh shieldu, finálna dokumentácia (Text, Schéma zapojenia, Strih videa)
+
+**Ivo Dovičák** <br>
+- užívateľské rozrhanie (display, encoder), preslov u videa 
+
+### Obsah
+
+* [Ciele projektu](#objectives)
+* [Popis Hardwaru](#hardware)
+* [Popis a simulácia zapojenia v prostredí SimulIDE](#modules)
+* [Video](#video)
+* [Literatura](#references)
+
+<a name="objectives"></a>
+
+## Ciele projektu
+
+- vytvoriť časovač s možnosťou nastavenia času.
+- využiť analógový joistick (2 kanály ADC, 1 tlačidlo) a enkóder pre ovládanie užívateľského rozhrania.
+- využiť vývojovú dosku Arduino UNO.
+- vytvoriť užívateľské rozhranie pomocou LCD displeja Digilent PmodCLP.
+- vytvoriť kód a využiť pri tom knižnice vytvorené počas laboratórnych cvičení.
+- vytoriť schému a návrh DPS v prostredí Altia (PCB Design Software).
+
+<a name="hardware"></a>
+
+## Popis Hardwaru
+### Arduino UNO, Enkóder, Joystick 
+
+Vrámci vývojovej sady bola použitá vývojová doska Arduino UNO R3, ktorá je založená na ATmega328. Doska obsahuje 14 digitálnych vstupných/výstupných kolíkov (z ktorých 6 môže byť použitých ako výstupy PWM), 6 analógových vstupov, kryštál 16 MHz, USB pripojenie, napájací konektor, rozhranie ICSP a tlačidlo resetu. Ďalej bol použitý rotačný enkóder typu KY-040, ktorému na jednu 360° otáčku pripadá 20 pulzov. Ako posledný komponent bol použitý joystick, ktorý sníma v 2 osiach (X a Y) do akej miery je vychýlený (má analógový výstup pre každú z osí). Joystick má aj zabudované tlačidlo (pin SW).
+
+### Shield
+
+- Do našeho studentského projektu jsme zakomponovali také vývojový kit - výstupní zesilovač s nastavitelným ziskem. Hlavní smysl našeho zesilovače je v rámci výuky přiblížit studentům samotný HW (ovšem vývojový kit od XILINX v našem případě deska Nexys A7-50T naprosto stačí).
+- Tento zesilovač by v případě realizace (GERBERY sú uvedené v prílohách projektu) sloužil na nastavení výstupní amplitudy signálu tj. větší zesílení zvuku.
+- Na vstupu zesilovače je zavedena ochrana proti nesprávnému zapojení napájecího napětí (polaritě),TVS dioda proti přesáhnutí maximálního napětí přivedeného na zesilovač a Low-pass filter pro potlačení rušení. 
+- Např. při napájení zesilovače z USB notebooku, můžeme dostat nasuperponované vysokofrekvenční rušení a tento filtr nám dokáže tyto složky potlačit.
+- Návrh zesilovače je realizovaný pomocí aplikace Altium Designer, kde po návrhnutí schématu se vytvořila DPS viz. obrázky níže.
+
+
+3D Model desky zesilovače <br> (Vrchní pohled) |Layoutový pohled desky zesilovače <br> (Vrchní pohled)
+:-------------------------:|:-------------------------:
+![278620852_1021410442135136_7154315500152959101_n](https://user-images.githubusercontent.com/99388246/165693223-0a94e4e5-78b2-440c-bc23-228c8eb99cf3.png)|![278885036_353535676806500_6518462935903523547_n](https://user-images.githubusercontent.com/99388246/165693208-ae4fd1d9-7659-4385-9e47-1a015b2743d0.png)
+
+3D Model desky zesilovače <br> (Boční pohled)|Layoutový pohled desky zesilovače <br> (Spodní pohled)
+:-------------------------:|:-------------------------:
+![TOP_LR_VIEW](https://user-images.githubusercontent.com/99397107/165839082-08e70d71-ed08-4b9f-8b42-5cba28c647ea.JPG)|![BOT_LR_VIEW](https://user-images.githubusercontent.com/99397107/165839125-298f1211-196a-41d6-ac05-d930292057f7.JPG)
+
+![0001](https://user-images.githubusercontent.com/99397107/165841715-2b7259a5-c591-4f51-b733-d8ca7df2b477.jpg)
+<fig caption> <p align="center"> Schéma desky zesilovače
+
+<a name="modules"></a>
+
+## Popis a simulace modulů psané ve VHDL
+
+### Modul Sin_gen 
+ 
+-	Modul sin_gen slúži na generáciu jednotlivých úrovní striedy, ktorou sa riadi PWM generátor. Vstupom sin generátora je výstupná frekvencia zo sw_handlera
+-	Sínus sme rozdelili na 36 častí, ktoré sme vytvorili pomocou excelu a pomocou signálu s_data_cnt volíme správnu hodnotu pre časový okamžik. Signálom s_treshold, ktorý je riadený vstupom FREQ ovládame periódu každého PWM pulzu a tým pádom aj frekvenciu výslednej sínusoidy. 
+
+#### Odkaz na kód modulu Sin-gen
+https://github.com/DominikCaban/de1_project_sinus/blob/main/projectdir/de1_projekt.srcs/sources_1/new/sin_generator.vhd
+
+### Modul SW handler 
+
+- Switch handler slúži na spravcovanie vstupu z prepínačov a jeho výstupom je frekvencia jednotlivých tónov. Jeho ďalším výstupom sú hodnoty data0_i a data1_i, ktoré slúžia na zobrzovanie aktuálne znejúceho tónu.
+
+#### Odkaz na kód modulu SW handler
+https://github.com/DominikCaban/de1_project_sinus/blob/main/projectdir/de1_projekt.srcs/sources_1/new/switch_handler.vhd
+
+### Modul PWM_gen
+
+- Modul PWM_GEN slúži na generáciu šírkovo modulovaného signálu. Vhodným postupným volením striedy vieme dostať rôzne typy signálov. V našom prípade ide o harmonický signál sínus.
+- Generovanie PWM signálov je jednoduché. Vedieme signál s_cnt, ktorým počítame časové impulzy a kontrolujeme či sme nedosiahli požadovanú striedu porovnávaním so signálom s_duty_old.
+ 
+#### Odkaz na kód modulu PWM_gen
+https://github.com/DominikCaban/de1_project_sinus/blob/main/projectdir/de1_projekt.srcs/sources_1/new/pwm_generator.vhd
+
+
+### Modul Driver-7seg
+
+- Modul driver_seg4 je upravenou verziou ovládača 7-segmentových displejov, ktoré sme využili z cvičení. Úprava spočívala v použití nového prevodníka z BCD na výstupy katód hex_7seg_letters , ktorý miesto čísiel zobrazoval tóny. Išlo o improvizovanú špeciálnu úpravu prevodníku hex_7seg, ktorý bol navrhnutý na cvičení DE1.
+
+#### Odkaz na kód modulu Driver-7seg
+https://github.com/DominikCaban/de1_project_sinus/blob/main/projectdir/de1_projekt.srcs/sources_1/new/driver_7seg_4digits.vhd
+
+### Výsledek testbench z prostředí Vivada
+
+Střed                      |Průchod nulou
+:-------------------------:|:-------------------------:
+![MIDDLE](https://user-images.githubusercontent.com/99397107/165825357-6f038a63-20d1-4ff8-b063-57b1a8716338.JPG)|![PrechodNulouFIN](https://user-images.githubusercontent.com/99397107/165826258-7d612401-1e23-45be-a742-2d1f50f00cf4.jpg)
+ 
+<a name="top"></a>
+
+## Popis a simulace TOP vrstvy ve VHDL
+
+TOP modul v sobě "skrývá" všechny moduly a umožnuje je připojit k hardwarových komponentům (v našem ppřípadě deska Nexys A7-50T)
+
+### Odkaz na kód TOP modulu
+https://github.com/DominikCaban/de1_project_sinus/blob/main/projectdir/de1_projekt.srcs/sources_1/new/top.vhd
+
+![TOP_Schema](https://user-images.githubusercontent.com/99397107/165823085-27287982-7e3d-4061-8c78-0134287f429d.JPG)
+|:--:| 
+|*Schéma Top modulu*|
+
+<a name="video"></a>
+### Příklad výstupních signálů (noty) zobrazených osciloskopicky
+
+Nota C4 <br> frekvence = ~261 Hz (Min)  |Nota C5 <br> frekvence = ~523 Hz (Max)
+:-------------------------:|:-------------------------:
+![C4](https://user-images.githubusercontent.com/99397107/165827889-ef8b4e22-0047-4556-83de-5ffc1c3d3463.png)|![C5](https://user-images.githubusercontent.com/99397107/165827930-9ff21b77-6a6a-4173-8dd5-d9f67f891dd6.png)
+
+### Nota C5: Min, Max, Střed a Spektrum sinusového signálu (oscilogramy)
+
+Minimum sinusoidy          |Maximum sinusoidy
+:-------------------------:|:-------------------------:
+![C5_MIN](https://user-images.githubusercontent.com/99397107/165830655-b29274e9-168a-4838-9db8-21de372fa88c.png)|![C5_MAX](https://user-images.githubusercontent.com/99397107/165830683-88cb7ced-67a9-4b66-be49-cc60672f50a2.png)
+
+Střed sinusoidy             |Spektrum sinusoidy
+:-------------------------:|:-------------------------:
+![C5_MID](https://user-images.githubusercontent.com/99397107/165830888-39846d89-9cd3-4b81-b038-4d63b277ad6b.png)|![C5_Spektrum](https://user-images.githubusercontent.com/99397107/165830906-6ef4ea90-2434-49d2-9d60-5067d2526a32.png)
+
+## Video 
+ 
+https://youtu.be/nLSVV8CweyI
+<a name="references"></a>
+
+## Diskuze o výsledcích projektu
+- Na snímkoch z ociloskopu možme pozorovať, že výsledna sínusoida nie je hladká, ale má "schodovitý tvar". Je to dané metódou, ktorú používame na generovanie sínusu, kedy si sínusoidu rozdelíme na 36 častí a každá z nich má istú úroveň. Hladšiu sínusoidu by sme dosiahli, ak by sme použili viacero častí, ale 36 častí pre naše účely postačuje.
+- Jeden z problémov, ktorý sme riešili, bol, že sme nevedeli vypočítať periódu PWM pulzov, pre ktoré by sme dosiahli požadovanú frekvenciu audio signálu. Po mnoha nevydarených výpočtoch sme použili experimentálnu metódu, kedy sme s pomocu osciloskopu prišli na periódu pwm pulzov, kedy má audio signál 1 kHz (bolo to 27770 ns).
+ Zvyšné frekvencie sme dopočítali podľa vzorca p = (2777/(f/1000)), dané riešenie fungovalo.
+- Ideálnější variantou by bylo využití VGA zesilovače, který by byl řiditelný přes vývojový kit, ovšem realizace v tomto konkrétním případě by byla komplikovaná a projekt by jen zkomplikovala.
+- Projekt nám přinesl vela nových skúseností, a naučili jsme se mnoho nových věcí. 
+
+## Literatura
+
+1. Nexys A7 Reference Manual: 
+- https://digilent.com/reference/programmable-logic/nexys-a7/reference-manual?redirect=1
+
+2. Github:
+- https://github.com/tomas-fryza/digital-electronics-1
+ 
+3. Sine wave generator using PWM:
+- https://www.ti.com/lit/an/spna217/spna217.pdf?
+ 
+4. Datasheet of operational amplifier:
+- https://www.analog.com/media/en/technical-documentation/data-sheets/ad8591_8592_8594.pdf
+
+5. Nexys A7-50T References: 
+- https://digilent.com/reference/_media/reference/programmable-logic/nexys-a7/nexys-a7-sch.pdf
+- https://digilent.com/reference/_media/reference/programmable-logic/nexys-a7/nexys-a7_rm.pdf
+6. Výsledné Altium soubory + Gerber
+- https://onedrive.live.com/?authkey=%21AFWKRFpeO7ds%2D%5FY&cid=3D59FBAD3CECD394&id=3D59FBAD3CECD394%2173346&parId=3D59FBAD3CECD394%2171179&action=locate
+
