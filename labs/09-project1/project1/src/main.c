5d95d114c47fbd6ad5df9d0f452e54a0697693fe
DominikCaban <caban.dominik@gmail.com>
2022-12-06 22:38:21 +0100

    Fix

--- labs/09-project1/project1/src/main.c
+++ labs/09-project1/project1/src/main.c
@@ -1,28 +1,28 @@
 /***********************************************************************
- *
+ * 
  * LCD Timer controlled by 2Axis Joystick and rotary encoder
  * Digital Electronics 2 Team Project 1
  * 5.12.2022
- *
- *
+ * 
+ * 
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
- *
- *
+ *  
+ * 
  * Based on code from Digital Electronics 2 Course
  * Copyright (c) 2018 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
- *
+ * 
  **********************************************************************/
 
 /* Includes ----------------------------------------------------------*/
-#include <avr/io.h>        // AVR device-specific IO definitions
-#include <avr/interrupt.h> // Interrupts standard C library for AVR-GCC
-#include <gpio.h>          // GPIO library for AVR-GCC
-#include "timer.h"         // Timer library for AVR-GCC
-#include <stdlib.h>        // C library. Needed for number conversions
-#include <uart.h>
-#include <lcd.h>
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include <gpio.h>           // GPIO library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include <stdlib.h>         // C library. Needed for number conversions
+#include <uart.h> 
+#include <lcd.h>   
 
 // rotary encoder
 #define ROT_SW 2
@@ -33,15 +33,17 @@
 #define JOY_SW 2
 
 // Global Variables
-uint8_t hours = 0, seconds = 0, minutes = 1, tenths = 0;                 // timer
-uint8_t set_hours = 0, set_seconds = 0, set_minutes = 1, set_tenths = 0; // timer
-uint8_t realx = 0, realy = 0;                                            // real x,y cursor position on lcd
-uint8_t rot_sw_state = 1, joy_sw_state = 1;                              // previos state to prevent multiple actions on single click
+uint8_t hours=0, seconds=0, minutes=1, tenths=0; // timer
+uint8_t set_hours=0, set_seconds=0, set_minutes=1, set_tenths=0; // timer
+uint8_t realx=0, realy=0; // real x,y cursor position on lcd
+uint8_t rot_sw_state=1, joy_sw_state=1; // previos state to prevent multiple actions on single click
 #define STARTED 1
 #define STOPPED 0
 uint8_t state = STOPPED;
 
-// Initialization of LCD display
+#define LED_GREEN 5
+
+//Initialization of LCD display
 void init_lcd()
 {
     // Initialize display
@@ -53,11 +55,11 @@ void init_lcd()
     lcd_gotoxy(5, 1);
     lcd_puts("STOPPED");
 
-    // Timer 2 is for LCD
+    //Timer 2 is for LCD
     TIM2_overflow_16ms();
 }
 
-// Initialization of Joystick functions (ADC)
+//Initialization of Joystick functions (ADC)
 void init_joystick()
 {
     // Timer1 is for starting ADC conversion
@@ -66,39 +68,39 @@ void init_joystick()
 
     // Configure Analog-to-Digital Convertion unit
     // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
-    ADMUX |= (1 << REFS0);  // 1
-    ADMUX &= ~(1 << REFS1); // 0
+    ADMUX |= (1<<REFS0); //1
+    ADMUX &= ~(1<<REFS1); //0
     // Select input channel ADC0 (voltage divider pin)
-    ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0)); // 0
+    ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0)); //0
     // Enable ADC module
-    ADCSRA |= (1 << ADEN);
+    ADCSRA |= (1<<ADEN);
     // Enable conversion complete interrupt
-    ADCSRA |= (1 << ADIE);
+    ADCSRA |= (1<<ADIE);
     // Set clock prescaler to 128
-    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
 
-    // set up SW button
-    DDRC &= ~(1 << JOY_SW);
-    PORTC |= (1 << JOY_SW);
+    //set up SW button
+    DDRC &= ~(1<<JOY_SW);
+    PORTC |= (1<<JOY_SW);
 }
 
-// Initialization of Rotary encoder
+//Initialization of Rotary encoder
 void init_encoder()
 {
     // Initialize Rotary Encoder:
-    DDRD &= ~(1 << ROT_SW);
-    PORTD |= (1 << ROT_SW);
+    DDRD &= ~(1<<ROT_SW);
+    PORTD |= (1<<ROT_SW);
 
-    DDRD &= ~(1 << ROT_CLK);
-    PORTD |= (1 << ROT_CLK);
+    DDRD &= ~(1<<ROT_CLK);
+    PORTD |= (1<<ROT_CLK);
 
-    DDRD &= ~(1 << ROT_DT);
-    PORTD |= (1 << ROT_DT);
-
+    DDRD &= ~(1<<ROT_DT);
+    PORTD |= (1<<ROT_DT);
+    
     // external interrupts
-    EIMSK = 1 << INT0;
-    EICRA = 1 << ISC00;
-    EICRA &= ~(1 << ISC01);
+    EIMSK = 1<<INT0;
+    EICRA = 1<<ISC00;
+    EICRA &= ~(1<<ISC01);
 }
 
 void cursor_off()
@@ -117,31 +119,31 @@ void refresh_lcdtime()
 {
     char string[2];
     cursor_off();
-    {
-        lcd_gotoxy(12, 0);
-
-        itoa(tenths, string, 10); // Convert decimal value to string
-        lcd_puts(string);
-    }
-
-    {
-        lcd_gotoxy(9, 0);
-        if (seconds < 10)
         {
-            lcd_puts("0");
+            lcd_gotoxy(12, 0);
+
+            itoa(tenths, string, 10); // Convert decimal value to string
+            lcd_puts(string);
         }
-        itoa(seconds, string, 10); // Convert decimal value to string
-        lcd_puts(string);
-    }
-    {
-        lcd_gotoxy(6, 0);
-        if (minutes < 10)
+
         {
-            lcd_puts("0");
+            lcd_gotoxy(9, 0);
+            if (seconds < 10)
+            {
+                lcd_puts("0");
+            }
+            itoa(seconds, string, 10); // Convert decimal value to string
+            lcd_puts(string);
         }
-        itoa(minutes, string, 10); // Convert decimal value to string
-        lcd_puts(string);
-    }
+        {
+            lcd_gotoxy(6, 0);
+            if (minutes < 10)
+            {
+                lcd_puts("0");
+            }
+            itoa(minutes, string, 10); // Convert decimal value to string
+            lcd_puts(string);
+        }
 
     lcd_gotoxy(3, 0);
     if (hours < 10)
@@ -153,33 +155,14 @@ void refresh_lcdtime()
     cursor_on();
 }
 
-void timer_runout(int i)
-{
-    if (i == 1)
-    {
-        PORTB |= (1 << PORTB5);
-    }
-    else if (i == 0)
-    {
-        PORTB &= ~(1 << PORTB5);
-    }
-    // if timer ran out - light up LED 
-}
-
-void start_tim()
-{
-    TIM2_overflow_interrupt_enable();
-    TIM1_overflow_interrupt_disable();
-    // disable rotary encoder interrupts
-    EIMSK &= (~1 << INT0);
-}
-
 // Start Clock Timer on LCD
 void start_timer()
 {
-    start_tim();
-    // save set values to reset back to
-    timer_runout(0);
+    TIM2_overflow_interrupt_enable();
+    TIM1_overflow_interrupt_disable();
+    //disable rotary encoder interrupts
+    EIMSK &= (~1<<INT0);
+    //save set values to reset back to
     set_hours = hours;
     set_minutes = minutes;
     set_seconds = seconds;
@@ -189,31 +172,29 @@ void start_timer()
     cursor_off();
 }
 
-void stop_tim()
-{
-    TIM2_overflow_interrupt_disable();
-    TIM1_overflow_interrupt_enable();
-    // enable rotary encoder interrupts
-    EIMSK = 1 << INT0;
-}
-
 // Stop Clock Timer on LCD
 void stop_timer()
 {
-    stop_tim();
+    TIM2_overflow_interrupt_disable();
+    TIM1_overflow_interrupt_enable();
+    //enable rotary encoder interrupts
+    EIMSK = 1<<INT0;
     lcd_gotoxy(5, 1);
     lcd_puts("STOPPED");
     cursor_on();
 }
 
-void reset_timer()
-{
+
+void timer_runout(){
+    //if timer ran out blink LED or buzzer
+     PORTB |= (1<<LED_GREEN);
+}
+
+void reset_timer(){
     hours = set_hours;
-    minutes = set_minutes;
+    minutes= set_minutes;
     seconds = set_seconds;
     tenths = set_tenths;
-    lcd_gotoxy(5, 1);
-    lcd_puts("START  ");
     refresh_lcdtime();
 }
 /*ADC  Channels
@@ -225,19 +206,22 @@ int main(void)
     // Initialize USART to asynchronous, 8N1, 9600
     uart_init(UART_BAUD_SELECT(9600, F_CPU));
     uart_puts("Start of project...\n");
-    DDRB |= (1 << DDB5);
+
     // Call all initialization functions
     init_lcd();
     init_joystick();
     init_encoder();
 
+    // init LED/buzzer
+    DDRB |= (1<<LED_GREEN);
+
     // Enables interrupts by setting the global interrupt mask
     sei();
-
+    
     // Infinite loop
     while (1)
     {
-        /* Empty loop. All subsequent operations are performed exclusively
+        /* Empty loop. All subsequent operations are performed exclusively 
          * inside interrupt service routines ISRs */
     }
 
@@ -245,63 +229,56 @@ int main(void)
     return 0;
 }
 
+
 ISR(INT0_vect)
-{
-    uint8_t clk, dt;
-    clk = (PIND & (1 << ROT_CLK)) >> ROT_CLK;
-    dt = (PIND & (1 << ROT_DT)) >> ROT_DT;
+{  
+    uint8_t sw, clk, dt;
+    sw = (PIND & (1<<ROT_SW));
+    clk = (PIND & (1<<ROT_CLK))>>ROT_CLK;
+    dt = (PIND & (1<<ROT_DT))>>ROT_DT;
     // SIMULIDE
     // In reality check if clk>dt or clk<dt
-    if (clk != dt)
+    if (clk!=dt)
     {
         uart_puts("LEFT");
         switch (realx)
         {
-        case 4:
-            if (hours > 0)
-                hours--;
-            break;
+            case 4:
+                if(hours>0) hours--;
+                break;
+            
+            case 7:
+               if(minutes>0)  minutes--;
+                break;
 
-        case 7:
-            if (minutes > 0)
-                minutes--;
-            break;
+            case 10:
+                if(seconds>0)  seconds--;
+                break;
 
-        case 10:
-            if (seconds > 0)
-                seconds--;
-            break;
-
-        case 12:
-            if (tenths > 0)
-                tenths--;
-            break;
+            case 12:
+               if(tenths>0)  tenths--;
+                break;
         }
-    }
-    else if (clk == dt)
+    }else if (clk==dt)
     {
         uart_puts("RIGHT");
         switch (realx)
         {
-        case 4:
-            if (hours < 99)
-                hours++;
-            break;
+            case 4:
+                if(hours<99) hours++;
+                break;
+            
+            case 7:
+                if(minutes<59) minutes++;
+                break;
 
-        case 7:
-            if (minutes < 59)
-                minutes++;
-            break;
+            case 10:
+                if(seconds<59) seconds++;
+                break;
 
-        case 10:
-            if (seconds < 59)
-                seconds++;
-            break;
-
-        case 12:
-            if (tenths < 9)
-                tenths++;
-            break;
+            case 12:
+                if(tenths<9) tenths++;
+                break;
         }
     }
     refresh_lcdtime();
@@ -314,37 +291,36 @@ ISR(TIMER1_OVF_vect)
     static uint8_t no_of_overflows = 0;
     no_of_overflows++;
 
-    uint8_t joy_sw = (PINC & (1 << JOY_SW)) >> JOY_SW;
-    uint8_t rot_sw = (PINB & (1 << ROT_SW)) >> ROT_SW;
+    uint8_t joy_sw = (PINC & (1<<JOY_SW)) >> JOY_SW;
+    uint8_t rot_sw = (PINB & (1<<ROT_SW)) >> ROT_SW;
     // Start timer
-    if (joy_sw == 0)
+    if (joy_sw==0)
     {
-        // do it only once per click
-        if (joy_sw_state == 1)
-        {
-            // otherwise start the clock
-            uart_puts("START  ");
+        //do it only once per click
+        if (joy_sw_state==1){
+            //otherwise start the clock
+            uart_puts("START");
             start_timer();
         }
     }
-    joy_sw_state = joy_sw;
+    joy_sw_state=joy_sw;
 
     // Reset timer back to set time
-    if (rot_sw == 0)
+    if (rot_sw==0)
     {
-        // do it only once per click
-        if (rot_sw_state == 1)
-        {
-            // otherwise start the clock
+        //do it only once per click
+        if (rot_sw_state==1){
+            //otherwise start the clock
             uart_puts("RESET");
+            reset_timer();
         }
     }
-    rot_sw_state = rot_sw;
+    rot_sw_state=rot_sw;
 
     if (no_of_overflows >= 3)
     {
-        no_of_overflows = 0;
-        ADCSRA |= (1 << ADSC);
+        no_of_overflows = 0;  
+        ADCSRA |= (1<<ADSC);
     }
 }
 
@@ -354,20 +330,24 @@ ISR(TIMER2_OVF_vect)
 {
     static uint8_t no_of_overflows = 0;
 
-    uint8_t joy_sw = (PINC & (1 << JOY_SW)) >> JOY_SW;
-    if (joy_sw == 0)
+    uint8_t joy_sw = (PINC & (1<<JOY_SW)) >> JOY_SW;
+    if (joy_sw==0)
     {
-        // do it only once per click
-        if (joy_sw_state == 1)
-        {
-            // otherwise start the clock
+        //do it only once per click
+        if (joy_sw_state==1){
+            //otherwise start the clock
             uart_puts("STOP");
             stop_timer();
         }
     }
-    joy_sw_state = joy_sw;
+    joy_sw_state=joy_sw;
 
-
+    /*uint8_t rot_sw = (PINB & (1<<ROT_SW));
+    if (rot_sw==0)
+    {
+        uart_puts("STOP");
+        stop_timer();
+    }*/
     no_of_overflows++;
     if (no_of_overflows >= 6)
     {
@@ -379,54 +359,47 @@ ISR(TIMER2_OVF_vect)
         {
             tenths = 9;
             seconds--;
-            if (seconds > 250)
-            {
-                seconds = 59;
-                minutes--;
-                if (minutes > 250)
+                if (seconds  > 250)
                 {
-                    minutes = 59;
-                    hours--;
-                    if (hours > 250)
+                    seconds = 59;
+                    minutes--;
+                    if (minutes  > 250)
                     {
-                        // timer done
-                        tenths = 0;  // Tenths of a second
-                        seconds = 0; // Seconds of a second
-                        minutes = 0; // Minutes of a second
-                        hours = 0;
-                        stop_tim();
-                        reset_timer();
-                        timer_runout(1);
+                        minutes = 59;
+                        hours--;
+                        if (hours  > 250){
+                            // timer done
+                            tenths = 0;  // Tenths of a second
+                            seconds = 0; // Seconds of a second
+                            minutes = 0; // Minutes of a second
+                            hours = 0;
+                            stop_timer();
+                            timer_runout();
+                        }
                     }
                 }
-            }
         }
     }
     refresh_lcdtime();
 }
 
-void cursor_change(int8_t x_direction, int8_t y_direction)
+void cursor_change(int8_t x_direction,int8_t y_direction)
 {
     char string[10];
-    static uint8_t vcursor_x = 0;
-    // static uint8_t vcursor_y=0; not needed for 2 lines, useful for multiline display
-    if (x_direction == 1 && vcursor_x < 3)
-    {
+    static uint8_t vcursor_x=0;
+    //static uint8_t vcursor_y=0; not needed for 2 lines, useful for multiline display
+    if (x_direction == 1 && vcursor_x<3){
         vcursor_x++;
     }
-    if (x_direction == -1 && vcursor_x > 0)
-    {
+    if (x_direction == -1 && vcursor_x>0){
         vcursor_x--;
     }
-    if (y_direction == 1)
-    {
-        realy = 1;
+    if (y_direction == 1){
+        realy=1;
+    }else if (y_direction==-1) {
+        realy=0;
     }
-    else if (y_direction == -1)
-    {
-        realy = 0;
-    }
-    uint8_t positions[] = {4, 7, 10, 12};
+    uint8_t positions[] = {4,7,10,12};
     realx = positions[vcursor_x];
 
     uart_puts("realx ");
@@ -442,52 +415,44 @@ void cursor_change(int8_t x_direction, int8_t y_direction)
 ISR(ADC_vect)
 {
     uint16_t value;
-    static uint8_t breakpoint = 0; // 0 neni breakpoint, 1 je breakpoint
+    // slovenska premenna pretoze ma nic ine nenapada, a dlho som nepisal po slovensky, ale mal by som zacat pisat bakalarku
+    static uint8_t doraz = 0; //0 neni doraz, 1 je doraz
 
     // Read converted value
     // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
-
+    
     value = ADC;
-    if ((ADMUX & 7) == 0)
-    {
+    if ((ADMUX & 7) == 0) {
         // X AXIS ADC
         if (value > 1000)
         {
-            if (breakpoint == 0)
-                cursor_change(1, 0);
-            breakpoint = 1;
-        }
-        else if (value < 30)
-        {
-            if (breakpoint == 0)
-                cursor_change(-1, 0);
-            breakpoint = 1;
-        }
-        else
-        {
-            // reset the breakpoint
-            breakpoint = 0;
+            if (doraz==0)
+            cursor_change(1,0);
+            doraz = 1;
+        }else if (value < 30){
+            if (doraz==0)
+            cursor_change(-1,0);
+            doraz = 1;
+        }else{
+            // nulujem doraz
+            doraz = 0;
         }
         // start reading y joystick position
         // Select input channel ADC1 (Y joystick)
-        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1));
-        ADMUX |= (1 << MUX0);
+        ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1));
+        ADMUX |= (1<<MUX0);
         // start conversion
-        ADCSRA |= (1 << ADSC);
-    }
-    else if ((ADMUX & 7) == 1)
-    {
+        ADCSRA |= (1<<ADSC);
+    }else if  ((ADMUX & 7)  == 1){
         // Y AXIS ADC
         // if y value is being read
         if (value > 1010)
         {
-            cursor_change(0, 1);
-        }
-        else if (value < 5)
-        {
-            cursor_change(0, -1);
+            cursor_change(0,1);
+        }else if (value < 5){
+            cursor_change(0,-1);
         }
         // select channel back to x input (channel 0)
-        ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+        ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
     }
 }
